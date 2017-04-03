43b9b55844cef4f002736828ae140f0682d806bc
Tyson J. LaFollette <tysonlafollette@gmail.com>
2017-04-02 22:21:50 -0600

    Updated code. Better git repository setup.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 //Simple terminal notes recorder. Made by Tyson LaFollette.
 
-#define _CRT_SECURE_NO_DEPRECATE
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -21,41 +20,42 @@ string timestamp()
 	return dt;
 }
 
-/*Catches next single paragraph user input. Inserts timestamp at beginning, then outputs to log.txt.*/
-int main(int argc, char *argv[])
-{
+void getAndWrite(bool should_append){
 	string entry;
-	string argument;
-	if (argc > 1){
-		argument = argv[1];
-		if (argument == "/?" || argument == "-?" || argument == "/h" || argument == "--h" || argument == "--help"){
-			cout << "Writes a single paragraph to a log file, timestamped with the current date and time." << endl << endl;
-			cout << "Log [/A] [/?]" << endl << endl;
-			cout << "/A\tAppends entry to log.txt without timestamp." << endl << endl;
-			cout << "/?\tShows this help document." << endl << endl;
-			cout << "More cool and minimalist stuff at www.tysonlafollette.com\n";
-		}
-		//If /a is used as argument, appends entry to previous, without a timestamp.
-		else if (argument == "/a"){
-			cout << "Log entry: ";
-			getline(cin, entry);
-			fstream fout;
-			fout.open("log.txt", fstream::out | fstream::app);
-			fout << entry << "\n\n";
-			fout.close();
-			cout << "Entry logged. See log.txt";
-		}
-	}
-	else
-	{
-		cout << "Log entry: ";
+	cout << "Log entry: ";
 		getline(cin, entry);
-		entry = timestamp() + entry;
+		if (!should_append) {entry = timestamp() + entry;}
 		fstream fout;
 		fout.open("log.txt", fstream::out | fstream::app);
 		fout << entry << "\n\n";
 		fout.close();
 		cout << "Entry logged. See log.txt";
+}
+
+/*Catches next single paragraph user input. Inserts timestamp at beginning, then outputs to log.txt.*/
+int main(int argc, char *argv[])
+{
+	string argument;
+	if (argc == 1) {//no command line parameters given.
+		 getAndWrite(false);
+	} else if (argc == 2){//possibly has a parameter.
+		argument = argv[1];
+		//check for help parameter
+		if (argument == "/?" || argument == "-?" || argument == "--?" || argument == "/h" || argument == "-h"
+            || argument == "--h" || argument == "/help" || argument == "-help" || argument == "--help"){
+			cout << "Writes a single paragraph to a log file, timestamped with the current date and time." << endl << endl;
+			cout << "Log [/A] [/?]" << endl << endl;
+			cout << "/a\tAppends entry to log.txt without timestamp." << endl << endl;
+			cout << "/?\tShows this help document." << endl << endl;
+			return 0;
+		}
+		//If /a is used as argument, appends entry to previous, without a timestamp.
+		else if (argument == "/a"){
+			getAndWrite(true);
+		}
+	} else {
+		cout << "Error: Too many parameters. Type --help for help.";
+		return 1;
 	}
 	return 0;
 }
